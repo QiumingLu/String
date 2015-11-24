94789892c3273d1b08a5431a91ba7ce3cb4df074
QiumingLu <cming_cs@163.com>
2015-11-24 13:54:19 +0800

    update

--- 1_5.cc
+++ 1_5.cc
@@ -1,12 +1,26 @@
+#include <stdio.h>
+
+struct Node {
+  int value;
+  Node* next;
+  Node() : value(0), next(NULL) { }
+  ~Node() { printf("free node\n"); }
+};
+
 // 如何在O(1)的时间里删除单链表的节点
-void DeleteNode(node* Linklist, node* p) {
-  printf("delete: %d\n", p->val);
+void DeleteNode(Node* Linklist, Node* p) {
+  printf("delete: %d\n", p->value);
   if (p->next != NULL) {
-    p->val = p->next->val;
-    node* tmp = p->next;
+    p->value = p->next->value;
+    Node* tmp = p->next;
     p->next = p->next->next;
     delete(tmp);
-  } else if (Linklist == p) {
+  } else {
+    Node* tmp = p;
+    p = NULL;
+    delete tmp;
+  }
+ /* else if (Linklist == p) {
     delete(p);
   } else {
     node *s = Linklist;
@@ -16,4 +30,13 @@ void DeleteNode(node* Linklist, node* p) {
     s->next = s->next->next;
     delete(p);
   }
+  */
+}
+
+int main() {
+  Node* p = new Node();
+  p->value = 2;
+  p->next = NULL;
+  DeleteNode(p, p);
+  return 0;
 }
